c14ff5572df0d8e5a8ddfabb945c652523c9cfec
TsukiSky <siqxiang@gmail.com>
2022-05-01 13:01:39 +0800

    developed basic UI framework

--- app/src/main/java/com/siqi/timestamp/MainActivity.java
+++ app/src/main/java/com/siqi/timestamp/MainActivity.java
@@ -1,17 +1,20 @@
 package com.siqi.timestamp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ViewPager2 viewPager;
-    private LinearLayout llCalendar, llToday, llProject;
     private ImageView ivCalendar, ivToday, ivProject, ivCurrent;
 
     @Override
@@ -23,15 +26,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initTabView() {
+        LinearLayout llCalendar = findViewById(R.id.id_tab_calendar);
+        llCalendar.setOnClickListener(this);
+        LinearLayout llToday = findViewById(R.id.id_tab_today);
+        llToday.setOnClickListener(this);
+        LinearLayout llProject = findViewById(R.id.id_tab_project);
+        llProject.setOnClickListener(this);
 
+        ivCalendar = findViewById(R.id.tab_iv_calendar);
+        ivToday = findViewById(R.id.tab_iv_today);
+        ivProject = findViewById(R.id.tab_iv_project);
+
+        ivToday.setSelected(true);
+        ivCurrent = ivToday;
     }
 
     private void initPage() {
+        viewPager = findViewById(R.id.id_viewpage);
+        ArrayList<Fragment> fragments = new ArrayList<>();
+//        fragments.add()
 
     }
 
+    private void changeTab(int pos){
+        ivCurrent.setSelected(false);
+
+        switch (pos){
+            case R.id.id_tab_calendar:
+                viewPager.setCurrentItem(0);
+                ivCalendar.setSelected(true);
+                ivCurrent = ivCalendar;
+                break;
+            case R.id.id_tab_today:
+                viewPager.setCurrentItem(1);
+                ivToday.setSelected(true);
+                ivCurrent = ivToday;
+                break;
+            case R.id.id_tab_project:
+                viewPager.setCurrentItem(2);
+                ivProject.setSelected(true);
+                ivCurrent = ivProject;
+        }
+    }
+
+
     @Override
     public void onClick(View view) {
-
+        changeTab(view.getId());
     }
 }
